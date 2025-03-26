58024532e76f977c2800259de739e80a3251622b
Tibor Hajszan <thajszan@t-online.hu>
2025-03-26 13:46:11 +0100

    Lesson "Exercise: Web App API" : Added Chuck image carousel and animations.

--- S22-24_BackEndApiProblemSolving/script.js
+++ S22-24_BackEndApiProblemSolving/script.js
@@ -1,17 +1,33 @@
 // Zero To Mastery Academy
 // Complete Web Developer in 2025: Zero to Mastery
-// Application Programming Interface :: Chuck Norris Jokes :: JavaScript
+// Application Programming Interface :: Chuck Norris Jokes :: Webpage Logic
 
-// joke text > new joke button > API URL -------------------------------------------------------------------------------
+// chuck image > joke text > new joke button > API URL -----------------------------------------------------------------
 
+const chuckImage = document.getElementById("chuck-image");
 const jokeText = document.getElementById("joke-text");
 const jokeBtn = document.getElementById("joke-btn");
 const apiUrl = "https://api.chucknorris.io/jokes/random";
 
-// fetching new joke from API ------------------------------------------------------------------------------------------
+// chuck image inits ---------------------------------------------------------------------------------------------------
+
+const chuckImages = [
+    "chuck1.jpg",
+    "chuck2.jpg",
+    "chuck3.jpg",
+    "chuck4.jpg",
+];
+let chuckIndex = 0;
+
+// cycling chuck image > fetching new joke > error handling ------------------------------------------------------------
 
 async function getNewJoke() {
     try {
+        // cycling chuck image
+        chuckIndex++;
+        if (3 < chuckIndex) {chuckIndex = 0;}
+        chuckImage.src = chuckImages[chuckIndex];
+        // fetching new joke
         const response = await fetch(apiUrl);
         const data = await response.json();
         jokeText.textContent = data.value;
