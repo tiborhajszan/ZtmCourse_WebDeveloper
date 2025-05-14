fc5f025311452ef71809da46827e4fd66bfd1bd1
Tibor Hajszan <thajszan@t-online.hu>
2025-05-14 16:20:32 +0200

    Lesson "Express Middleware" : Completed.

--- S26_NodeJsExpressJs/express-server.js
+++ S26_NodeJsExpressJs/express-server.js
@@ -1,26 +1,29 @@
 // #####################################################################################################################
 // Zero To Mastery Academy
 // Complete Web Developer in 2025: Zero to Mastery
-// Back End | Servers | Server With Express Package
+// Back End | Servers | Server With Express Package, Express Middleware
 // #####################################################################################################################
 
 const express = require("express");
 const api = express();
 api.listen(3000);
 
+// express middleware ##################################################################################################
+
+api.use((request, response, next) => {
+  console.log("I am a middleware!");
+  next();
+});
+
 // html server #########################################################################################################
 
-function htmlServer(request, response) {
+api.get("/", (requst, response) => {
   response.send("<h1>Hello World!</h1>");
-}
-
-api.get("/", htmlServer);
+});
 
 // json server #########################################################################################################
 
-function jsonServer(request, response) {
+api.get("/user", (request, response) => {
   const user = {name: "John Doe", hobby: "Coding"}
   response.send(user);
-}
-
-api.get("/user", jsonServer);
+});
