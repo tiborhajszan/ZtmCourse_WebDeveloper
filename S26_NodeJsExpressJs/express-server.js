7e1cc75a3cc058e64edc28394311745b5dc0016f
Tibor Hajszan <thajszan@t-online.hu>
2025-05-14 15:45:01 +0200

    Lesson "Introduction to Express.js" : Completed.

--- S26_NodeJsExpressJs/express-server.js
+++ S26_NodeJsExpressJs/express-server.js
@@ -1,30 +1,26 @@
 // #####################################################################################################################
 // Zero To Mastery Academy
 // Complete Web Developer in 2025: Zero to Mastery
-// Node.js + Express.js | Building a Server | Server With Http Module
+// Back End | Servers | Server With Express Package
 // #####################################################################################################################
 
-const http = require("http");
+const express = require("express");
+const api = express();
+api.listen(3000);
 
 // html server #########################################################################################################
 
 function htmlServer(request, response) {
-  response.setHeader("Content-Type", "text/html");
-  response.write("<h1>Hello World!</h1>");
-  response.write("URL: " + request.url + "<br />");
-  response.end("method: " + request.method);
+  response.send("<h1>Hello World!</h1>");
 }
 
-const htmlAPI = http.createServer(htmlServer);
-htmlAPI.listen(3000);
+api.get("/", htmlServer);
 
 // json server #########################################################################################################
 
 function jsonServer(request, response) {
   const user = {name: "John Doe", hobby: "Coding"}
-  response.setHeader("Content-Type", "application/json");
-  response.end(JSON.stringify(user));
+  response.send(user);
 }
 
-const jsonAPI = http.createServer(jsonServer);
-jsonAPI.listen(3001);
+api.get("/user", jsonServer);
