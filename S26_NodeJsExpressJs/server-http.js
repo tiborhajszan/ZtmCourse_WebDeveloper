7b251f5521e82c40b2539bc5882dd54967dc69fc
Tibor Hajszan <thajszan@t-online.hu>
2025-06-06 12:07:20 +0200

    Lesson "Building a Server" : Updated notes and code.

--- S26_NodeJsExpressJs/server-http.js
+++ S26_NodeJsExpressJs/server-http.js
@@ -1,30 +1,34 @@
-// #####################################################################################################################
-// Zero To Mastery Academy
-// Complete Web Developer in 2025: Zero to Mastery
-// Node.js + Express.js | Building a Server | Server With Http Module
-// #####################################################################################################################
+//######################################################################################################################
+//  Zero To Mastery Academy
+//  Complete Web Developer in 2025: Zero to Mastery
+//  Node.js + Express.js | Server Using Http Library
+//######################################################################################################################
 
 const http = require("http");
 
-// html server #########################################################################################################
+// html api ############################################################################################################
 
+// html server function ------------------------------------------------------------------------------------------------
 function htmlServer(request, response) {
   response.setHeader("Content-Type", "text/html");
   response.write("<h1>Hello World!</h1>");
-  response.write("URL: " + request.url + "<br />");
-  response.end("method: " + request.method);
-}
+  response.write(`<p>URL: ${request.url}</p>`);
+  response.end(`<p>Method: ${request.method}</p>`);
+};
 
+// creating html api ---------------------------------------------------------------------------------------------------
 const htmlAPI = http.createServer(htmlServer);
 htmlAPI.listen(3000);
 
-// json server #########################################################################################################
+// json api ############################################################################################################
 
+// json server function ------------------------------------------------------------------------------------------------
 function jsonServer(request, response) {
-  const user = {name: "John Doe", hobby: "Coding"}
+  const user = {name: "John Doe", hobby: "Coding"};
   response.setHeader("Content-Type", "application/json");
   response.end(JSON.stringify(user));
-}
+};
 
+// creating json api ---------------------------------------------------------------------------------------------------
 const jsonAPI = http.createServer(jsonServer);
 jsonAPI.listen(3001);
